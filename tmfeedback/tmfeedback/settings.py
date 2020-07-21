dfed226820f83ffef1cf867183071232f3eb33cd
Andrew Whitaker <a1whitak@eng.ucsd.edu>
2020-07-21 06:53:17 -0700

    Logistical changes: using decouple to protect my secure data in settings.py and introduced a requirements.txt

--- tmfeedback/tmfeedback/settings.py
+++ tmfeedback/tmfeedback/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config, Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=b=7916+k*6w&%38o66(+hwp4l%!!adj$9o_gmq#v=lc73d%_-'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 AUTH_USER_MODEL = 'users.User'
 
