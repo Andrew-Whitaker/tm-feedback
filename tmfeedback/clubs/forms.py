435e9e2ee356e0ee1455436c8872991ce81f16ec
Andrew Whitaker <a1whitak@eng.ucsd.edu>
2020-07-30 15:06:25 -0700

    Created Membership Request management view. All pending membership requests show up here. No security to only allow the club organizer access

--- tmfeedback/clubs/forms.py
+++ tmfeedback/clubs/forms.py
@@ -3,3 +3,13 @@ from django import forms
 
 class MembershipRequestForm(forms.Form):
     pass
+
+
+class MembershipApprovalForm(forms.Form):
+    CHOICES = [('Y', 'Approve'), ('N', 'Deny')]
+    request_decision = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
+
+
+def create_membership_request_formset(form_count):
+    return forms.formset_factory(MembershipApprovalForm, extra=form_count)
+
--- tmfeedback/clubs/views.py
+++ tmfeedback/clubs/views.py
@@ -3,10 +3,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, View
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.forms import formset_factory
 import sys
 
 from .models import Club
-from .forms import MembershipRequestForm
+from .forms import MembershipRequestForm, MembershipApprovalForm, create_membership_request_formset
 
 
 class ClubIndexView(ListView):
@@ -63,6 +64,57 @@ class ClubMemberRoster(LoginRequiredMixin, ListView):
         queryset = self.club.members.order_by('-last_name')
         return queryset
 
-# class ClubOrganizer
+
+class ClubManageRequestsView(LoginRequiredMixin, View):
+
+    def get(self, request, club_id):
+        club = get_object_or_404(Club, pk=club_id)
+        requesters = get_user_model().objects.filter(pending_clubs__pk=club_id)
+        request_count = requesters.count()
+        formset = create_membership_request_formset(request_count)()
+
+        request_forms = zip(formset, requesters)
+        context = {'form_requests': request_forms,
+                   'club': club,
+                   'formset': formset,
+                   'request_count': request_count
+                   }
+        return render(request, 'clubs/club_manage_requests.html', context)
+
+    def post(self, request, club_id, **kwargs):
+        club = get_object_or_404(Club, pk=club_id)
+        requesters = get_user_model().objects.filter(pending_clubs__pk=club_id)
+        request_count = requesters.count()
+        formset = create_membership_request_formset(request_count)(request.POST)
+
+        for form, user in zip(formset, requesters):
+            if form.is_valid():
+                data = form.cleaned_data
+                self.fulfill_membership_decision(data, user, club)
+
+        return redirect('club_member_roster', club_id=club_id)
+
+    @staticmethod
+    def fulfill_membership_decision(form_data, user, club):
+        response_key = 'request_decision'
+        if response_key in form_data.keys():
+            if form_data[response_key] == 'Y':
+                club.membership_requests.remove(user)
+                club.members.add(user)
+                club.save()
+            else:
+                club.membership_requests.remove(user)
+                club.save()
+        return
+        # request_count = int(request.context['request_count'])
+        #
+        # formset = create_membership_request_formset(request_count)(request.POST)
+        # for
+        #     clean_data = formset.cle
+        #
+
+
+
+
 
 
