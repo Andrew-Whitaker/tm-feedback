78359e7e89bf13d87573ac8e72bf089999362a74
Andrew Whitaker <a1whitak@eng.ucsd.edu>
2020-08-08 11:12:51 -0700

    Introduced Performance Model into the system. Has reference to its meeting, performer, and role

--- tmfeedback/meetings/models.py
+++ tmfeedback/meetings/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.conf import settings
 from datetime import date as dt
 from django.shortcuts import reverse
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 from clubs.models import Club
 
@@ -21,4 +23,32 @@ class Meeting(models.Model):
         }
         return reverse('meeting_detail', kwargs=kwargs)
 
+
+def get_default_user():
+    return get_user_model().get_deleted_default_user()
+
+
+class Performance(models.Model):
+    meeting = models.ForeignKey(Meeting, on_delete=models.CASCADE, related_name='performances')
+    performer = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                  on_delete=models.SET(get_default_user))
+
+    # Performance Roles
+    class Role(models.TextChoices):
+        TOASTMASTER = 'TMD', _('Toastmaster')
+        TABLE_TOPICS_MASTER = 'TTM', _('Table Topics Master')
+        EVALUATOR = 'EVAL', _('Evaluator')
+        GENERAL_EVALUATOR = 'GE', _('General Evaluator')
+        SPEAKER = 'S', _('Speaker')
+        TABLE_TOPICS_SPEAKER = 'TTS', _('Table Topics Speaker')
+
+    role = models.CharField(max_length=4, choices=Role.choices, default=Role.SPEAKER)
+
+    def __str__(self):
+        return '{date} {role} {user}'.format(date=dt.strftime(self.meeting.date, "%m-%d-%y"),
+                                             role=self.get_role_label(),
+                                             user=self.performer.first_name)
+
+    def get_role_label(self):
+        return self.Role(self.role).label
 
--- tmfeedback/users/models.py
+++ tmfeedback/users/models.py
@@ -6,3 +6,12 @@ class User(AbstractUser):
 
     def get_full_name(self):
         return self.first_name + ' ' + self.last_name
+
+    @staticmethod
+    def get_deleted_default_user():
+        (user, created) = User.objects.get_or_create(
+            first_name='Deleted',
+            last_name='User',
+            username='deleted_user'
+        )
+        return user
