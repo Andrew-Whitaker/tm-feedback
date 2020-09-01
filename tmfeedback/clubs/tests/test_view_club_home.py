79d8afbb0fb02f6801d65af14e856fea1986492f
Andrew Whitaker <a1whitak@eng.ucsd.edu>
2020-09-01 10:36:50 -0700

    Fixed Urls on the club home page and introduced new tests for the navigation and breadcrumb links

--- tmfeedback/clubs/tests/test_view_club_home.py
+++ tmfeedback/clubs/tests/test_view_club_home.py
@@ -4,33 +4,58 @@ from django.contrib.auth import get_user_model
 
 from ..views import ClubHomeView
 from ..models import Club
+from meetings.models import Meeting
 
 
 class ClubHomeTests(TestCase):
     def setUp(self):
         self.club = Club.objects.create(name='Test Club', id=1, description='Club for testing')
+        self.meeting = Meeting.objects.create(date='2020-08-28', theme='Integrity', club=self.club)
         get_user_model().objects.create_user(username='john', email='john@doe.com', password='12345678')
         self.client.login(username='john', password='12345678')
+        self.url = reverse('clubs:home', kwargs={'club_id': 1})
+        self.response = self.client.get(self.url)
 
     def test_club_home_view_success_status_code(self):
-        url = reverse('clubs:home', kwargs={'club_id': 1})
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(self.response.status_code, 200)
 
     def test_club_home_view_not_found_status_code(self):
-        url = reverse('clubs:home', kwargs={'club_id': 13})
-        response = self.client.get(url)
+        bad_url = reverse('clubs:home', kwargs={'club_id': 13})
+        response = self.client.get(bad_url)
         self.assertEquals(response.status_code, 404)
 
     def test_club_home_url_resolves_club_home_view(self):
         view = resolve('/clubs/1/')
         self.assertEquals(view.func.view_class, ClubHomeView)
 
-    def test_club_home_view_contains_nav_links(self):
-        url = reverse('clubs:home', kwargs={'club_id': self.club.id})
+    def test_club_home_view_contains_breadcrumb_links(self):
+        club_index_url = reverse('clubs:index')
+        self.assertContains(self.response, 'href="{0}"'.format(club_index_url))
+
+    def test_club_home_view_contains_meeting_nav_links(self):
+        # There should be a link to access the full list of meetings the club has had
+        meeting_index_url = reverse('clubs:meeting_index', kwargs={'club_id': self.club.id})
+        self.assertContains(self.response, 'href="{0}"'.format(meeting_index_url))
+
+        # There should be a link that redirects to the meeting create page for a club
+        meeting_creation_url = reverse('clubs:create_meeting', kwargs={'club_id': self.club.id})
+        self.assertContains(self.response, 'href="{0}"'.format(meeting_creation_url))
+
+        # There should be at least one meeting in the short meeting list with a link
+        # to that meeting detail page
+        meeting_detail_url = reverse('meetings:detail', kwargs={'meeting_pk': 1})
+        self.assertContains(self.response, 'href="{0}"'.format(meeting_detail_url))
+
+
+    def test_club_home_view_contains_member_nav_links(self):
+        # There should be a link to access the full member roster
         roster_url = reverse('clubs:member_roster', kwargs={'club_id': self.club.id})
-        response = self.client.get(url)
-        self.assertContains(response, 'href="{0}"'.format(roster_url))
+        self.assertContains(self.response, 'href="{0}"'.format(roster_url))
+
+        # There should be at least one member in the short roster list with a link
+        # to their user profile page
+        self.assertTrue(False, 'Have not created links to user profiles from club home page.')
+        # TODO: Make a real test once user profiles have been implemented
 
 
 class ClubHomeMembershipButtonTests(TestCase):
